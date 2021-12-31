96bb2b19d8ee85dee7e95a6947b18957fc9d56c8
stksw <kakiage@gmail.com>
2021-12-31 16:02:17 +0900

    modify context processors

--- config/custom_context_processors.py
+++ config/custom_context_processors.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from base.models import Item
 
+# どのページからでも読み込み可能になる
 def base(request):
     items = Item.objects.filter(is_published=True)
     return {
         'TITLE': settings.TITLE,
-        'ADDITIONAL_ITEM': items,
+        'ADDITIONAL_ITEMS': items,
         'POPULAR_ITEMS': items.order_by('-sold_count') # -をつけることで降順でソート
     }
